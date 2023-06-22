3907ba1684869fada124b8aa39191f1da4d9cee2
Sebastian Klement <Sebastian.Klement@HS-Augsburg.de>
2023-06-22 16:46:55 -0700

    update

--- src/display.py
+++ src/display.py
@@ -1,114 +1,43 @@
-from machine import Pin, I2C, Timer, SPI
-from lib.ssd1306 import SSD1306_SPI
+from machine import Pin, SPI
+from ssd1306 import SSD1306_SPI
 import utime
 
-SCREEN_WIDTH = 128  # number of columns
-SCREEN_HEIGHT = 64  # number of rows
+class OLEDText:
+    def __init__(self, width=128, height=64, spi_device=1, sck=10, mosi=11, res=12, dc=14, cs=13):
+        self.width = width
+        self.height = height
+        self.spi_device = spi_device
+        
+        # Initialize the SPI
+        spi_sck = Pin(sck)  
+        spi_sda = Pin(mosi)
+        spi_res = Pin(res) 
+        spi_dc  = Pin(dc)
+        spi_cs  = Pin(cs)
+        
+        self.oled_spi = SPI(spi_device, baudrate=100000, sck=spi_sck, mosi=spi_sda)
+        self.oled = SSD1306_SPI(self.width, self.height, self.oled_spi, spi_dc, spi_res, spi_cs, True)
 
-spi_sck = Pin(10)  # sck stands for serial clock; always be connected to SPI SCK pin of the Pico
-spi_sda = Pin(11)  # sda stands for serial data;  always be connected to SPI TX pin of the Pico; this is the MOSI
-spi_res = Pin(12)  # res stands for reset; to be connected to a free GPIO pin
-spi_dc  = Pin(14)  # dc stands for data/command; to be connected to a free GPIO pin
-spi_cs  = Pin(13)  # chip select; to be connected to the SPI chip select of the Pico 
+    def set_text(self, text, row=0):
+        self.oled.text(text, 0, row * 10)  # Assumes each row is 10 pixels high
 
-SPI_DEVICE = 1  # Because the peripheral is connected to SPI 0 hardware lines of the Pico
+    def display_text_value(self, text, value, row=0):
+        full_text = "{}: {}".format(text, value)
+        self.clear()  # Clear the display first
+        self.set_text(full_text, row)  # Set the full text
+        self.update_display()  # Update the display
 
-oled_spi = SPI( SPI_DEVICE, baudrate= 100000, sck= spi_sck, mosi= spi_sda )
-oled = SSD1306_SPI( SCREEN_WIDTH, SCREEN_HEIGHT, oled_spi, spi_dc, spi_res, spi_cs, True)
+    def update_display(self):
+        self.oled.show()
 
-# State machine states
-CLOCK_DISPLAY = 0
-FORMAT_SETTING = 1
-ALARM_SETTING = 2
-VOLUME_SETTING = 3
-RADIO_STATION_SETTING = 4
-SETTINGS = [
-    "Time",
-    "Format",
-    "Alarm",
-    "Volume",
-    "Station"
-]
-NUM_SETTINGS = len(SETTINGS)
+    def clear(self):
+        self.oled.fill(0)
 
-# Buttons
-mode_button = Pin(2, Pin.IN, Pin.PULL_UP)  # Assuming button on GPIO pin 2
-select_button = Pin(3, Pin.IN, Pin.PULL_UP)  # Assuming button on GPIO pin 3
-left_button = Pin(4, Pin.IN, Pin.PULL_UP)  # Assuming button on GPIO pin 4
-right_button = Pin(5, Pin.IN, Pin.PULL_UP)  # Assuming button on GPIO pin 5
-
-# Settings
-format_24h = True
-alarm_time = '06:00'
-volume = 5
-radio_station = '100.0 FM'
-settings_values = [
-    utime.localtime(),
-    format_24h,
-    alarm_time,
-    volume,
-    radio_station
-]
-
-# State
-state = CLOCK_DISPLAY
-changing_setting = False
-
-def mode_button_pressed(_):
-    global state, changing_setting
-    if not changing_setting:
-        state = (state + 1) % NUM_SETTINGS
-    oled.fill(0)
-    update_display()
-
-def select_button_pressed(_):
-    global changing_setting
-    changing_setting = not changing_setting
-    oled.fill(0)
-    update_display()
-
-def left_button_pressed(_):
-    if changing_setting:
-        change_setting(-1)
-    oled.fill(0)
-    update_display()
-
-def right_button_pressed(_):
-    if changing_setting:
-        change_setting(1)
-    oled.fill(0)
-    update_display()
-
-def change_setting(delta):
-    global format_24h, alarm_time, volume, radio_station
-    if state == FORMAT_SETTING:
-        format_24h = not format_24h
-    elif state == ALARM_SETTING:
-        # Implement logic for changing alarm time
-        pass
-    elif state == VOLUME_SETTING:
-        volume = max(0, volume + delta)
-    elif state == RADIO_STATION_SETTING:
-        # Implement logic for changing radio station
-        pass
-    settings_values[state] = format_24h if state == FORMAT_SETTING else settings_values[state]
-    settings_values[state] = alarm_time if state == ALARM_SETTING else settings_values[state]
-    settings_values[state] = volume if state == VOLUME_SETTING else settings_values[state]
-    settings_values[state] = radio_station if state == RADIO_STATION_SETTING else settings_values[state]
-
-def update_display():
-    if changing_setting:
-        oled.text("Change {} to {}".format(SETTINGS[state], settings_values[state]), 0, 0)
-    else:
-        oled.text("Time: {}".format(settings_values[0]), 0, 0)
-        for i in range(1, NUM_SETTINGS):
-            oled.text("{}: {}".format(SETTINGS[i], settings_values[i]), 0, (i * 10) % SCREEN_HEIGHT)
-    oled.show()
-
-mode_button.irq(mode_button_pressed, Pin.IRQ_FALLING)
-select_button.irq(select_button_pressed, Pin.IRQ_FALLING)
-left_button.irq(left_button_pressed, Pin.IRQ_FALLING)
-right_button.irq(right_button_pressed, Pin.IRQ_FALLING)
-
-timer = Timer()
-timer.init(period=1000, mode=Timer.PERIODIC, callback=lambda t:update_display())  # Update every second
+    def clear_display(self):
+        self.fill(0)
+        self.show()
+        
+    def display_time(self):
+        current_time = utime.localtime()
+        time_str = "{:02d}:{:02d}:{:02d}".format(current_time[3], current_time[4], current_time[5])
+        self.display_text_value("Time", time_str)
--- src/main.py
+++ src/main.py
@@ -1,21 +1,30 @@
 import sys
 import time
 import display
-import radio
-import clock
 import utime
+from machine import Pin, I2C, Timer, SPI
+
 
 print("starting clockradio")
 utime.sleep(0.5)
 
+# Buttons
+mode_button = Pin(0, Pin.IN, Pin.PULL_UP)  # Assuming button on GPIO pin 2
+select_button = Pin(1, Pin.IN, Pin.PULL_UP)  # Assuming button on GPIO pin 3
+left_button = Pin(2, Pin.IN, Pin.PULL_UP)  # Assuming button on GPIO pin 4
+right_button = Pin(3, Pin.IN, Pin.PULL_UP)  # Assuming button on GPIO pin 5
+
+oled = display.OLEDText()
+
+
 try:
     # Start the display
-    display.update_display()
-
+    
     # Main loop
     while True:
         # Add tasks here
         utime.sleep(1)
+        oled.display_time()
 
 except Exception as e:
     print('Error:', e)
